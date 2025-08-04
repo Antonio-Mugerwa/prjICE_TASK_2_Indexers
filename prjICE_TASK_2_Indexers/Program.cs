5c6352bad92df680fd9d05ab19ae06e44745ebe7
Moony2099 <160716037+Moonlite3751@users.noreply.github.com>
2025-08-04 13:50:00 +0200

    Added my 20 cents as specified in class

--- prjICE_TASK_2_Indexers/Program.cs
+++ prjICE_TASK_2_Indexers/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Movies movie = new Movies("Avengers Infinity War", "The Russso Brothers", "Action", 2018, 85);
+            movie[2] = "Superhero Action";
+
+            Console.WriteLine(movie[0]); // Output: Avengers Infinity War
+            Console.WriteLine(movie[1]); //Output: The Russso Brothers
+            Console.WriteLine(movie[2]); // Output: Superhero Action
+            Console.WriteLine(movie[3]); // Output: 2018
+            Console.WriteLine(movie[4]); // Output: 85
+
+            Movies movies = new Movies("Avengers Endgame", "The Russso Brothers", "Sci-fi", 2018, 94);
+            movies["movieGenre"] = "Sci-fi & Superhero Action";
+
+            Console.WriteLine($"Movie title: {movies["movieTitle"]}"); // Output: Avengers Endgame
+            Console.WriteLine($"Movie director: {movies["movieDirector"]}"); // Output: The Russso Brothers
+            Console.WriteLine($"Movie genre: {movies["movieGenre"]}"); // Output: Sci-fi & Superhero Action
+            Console.WriteLine($"Release year: {movies["releaseYear"]}"); // Output: 2018
+            Console.WriteLine($"Rotten tomato score: {movies["reviewScore"]}"); // Output: 94
         }
     }
 }
